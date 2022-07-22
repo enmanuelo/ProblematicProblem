71f1d6653ad227e7ac22c9a8bdfebf5bea6231ba
Enmanuel Ozoria <ozoriaeman1998@gmail.com>
2022-07-22 13:27:47 -0400

    fixed remaining runtime erros associated with the values assigned to boolean variables addToList and cont

--- ProblematicProblem/Program.cs
+++ ProblematicProblem/Program.cs
@@ -13,7 +13,7 @@ namespace ProblematicProblem
         {
             Random rng = new Random();
             Console.Write("Hello, welcome to the random activity generator! \nWould you like to generate a random activity? yes/no: ");
-            cont = bool.Parse(Console.ReadLine());
+            cont = Console.ReadLine().ToLower() == "yes" ? true : false;            
             Console.WriteLine();
             Console.Write("We are going to need your information first! What is your name? ");
             string userName = Console.ReadLine();
@@ -22,7 +22,7 @@ namespace ProblematicProblem
             int userAge = int.Parse(Console.ReadLine());
             Console.WriteLine();
             Console.Write("Would you like to see the current list of activities? Sure/No thanks: ");
-            bool seeList = bool.Parse(Console.ReadLine());
+            bool seeList = Console.ReadLine().ToLower() == "sure" ? true : false;
             if (seeList)
             {
                 foreach (string activity in activities)
@@ -32,7 +32,7 @@ namespace ProblematicProblem
                 }
                 Console.WriteLine();
                 Console.Write("Would you like to add any activities before we generate one? yes/no: ");
-                bool addToList = bool.Parse(Console.ReadLine());
+                bool addToList = Console.ReadLine().ToLower() == "yes" ? true : false;
                 Console.WriteLine();
                 while (addToList)
                 {
@@ -46,7 +46,7 @@ namespace ProblematicProblem
                     }
                     Console.WriteLine();
                     Console.WriteLine("Would you like to add more? yes/no: ");
-                    addToList = bool.Parse(Console.ReadLine());
+                    addToList = Console.ReadLine().ToLower() == "yes" ? true : false;
                 }
             }
 
@@ -82,7 +82,7 @@ namespace ProblematicProblem
                 }
                 Console.Write($"Ah got it! {userName}, your random activity is: {randomActivity}! Is this ok or do you want to grab another activity? Keep/Redo: ");
                 Console.WriteLine();
-                bool cont = bool.Parse(Console.ReadLine());
+                cont = Console.ReadLine().ToLower() == "redo" ? true : false;
             }
         }
     }
